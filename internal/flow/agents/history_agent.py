4fd6a3a2ba2a9b5affaafe82e7213069bf8bb767
stephen-personamatic <stephen@personamatic.ai>
2023-04-07 18:39:54 -0500

    list user chat messages isn't static function, so must be called by instatiated object. "self" here refers to the history agent, not the conversation service.
    Track message length as messages are being added. If max length is reached, chunk message

--- internal/flow/agents/history_agent.py
+++ internal/flow/agents/history_agent.py
@@ -9,22 +9,34 @@ from internal.services.dynamodb_service import DynamoDBService
 from typing import List
 
 class HistoryAgent(BaseAgent):
-    def __init__(self, agent, consumer) -> None:
+    def __init__(self, agent, consumer, max_size=1000) -> None:
         super().__init__(agent, consumer)
 
         # self.chat_service = ChatService()
         self.conversation_service = ConversationService()
         self.chat_dynamodb_service = DynamoDBService('ChatMessage')
 
+        self.max_size = max_size
+
     def consume(self, chat_message: ChatMessage, response_dict):
+        # TODO: Make sure that the chat message are sorted by most recent first
+
         # get list of messages from dynamodb
         # TODO: handle pagination for large conversations
-        result = ConversationService.list_user_chat_messages(self, chat_message.conversation_id, chat_message.user_id)
+        result = self.conversation_service.list_user_chat_messages(chat_message.conversation_id, chat_message.user_id)
         data = result.Item
 
         # create a list of chat messages
         messages = []
+        total_length = 0
         for message in data:
+            message_length = len(message)
+
+            # Only include portion of message if greater than max length
+            if total_length + message_length > self.max_size:
+                message = message[:total_length - message_length]
+                message_length = len(message)
+
             # for each dict message, create a ChatMessage object
             new_chat_message = ChatMessage(
                 id = message.get('id'),
@@ -36,11 +48,10 @@ class HistoryAgent(BaseAgent):
                 agent_name = message.get('agent_name'),
                 message = message.get('message'),
             )
+
             messages.append(new_chat_message)
+            total_length += message_length
 
         # append the latest chat message
         messages.append(chat_message)
-
-        # TODO: messages should not exceed a certain max token size, so cut off the oldest messages. oldest message is the first message in the list
-
         return self.broadcast(messages, response_dict)
